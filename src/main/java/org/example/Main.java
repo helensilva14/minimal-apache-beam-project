3b27977b5a6a6ef88cb86bfe789e1586d5270908
Helen Cristina Ferreira <helencristina@google.com>
2023-02-09 16:02:22 +0000

    Add queries and test PTransforms

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,12 +3,15 @@ package org.example;
 import com.google.api.services.bigquery.model.TableRow;
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.PipelineResult;
+import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.io.gcp.bigquery.BigQueryIO;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.transforms.Flatten;
+import org.apache.beam.sdk.transforms.MapElements;
 import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.sdk.values.PCollectionList;
+import org.apache.beam.sdk.values.TypeDescriptors;
 
 public class Main {
     public static void main(String[] args) {
@@ -18,20 +21,17 @@ public class Main {
 
         Pipeline pipeline = Pipeline.create(options);
 
-        PCollection<TableRow> tableA = pipeline
-                .apply(
-                        "Read from BigQuery table A",
-                        BigQueryIO.readTableRows().withoutValidation().from(""));
+        PCollection<TableRow> tableA = pipeline.apply("Read from BigQuery table A",
+                BigQueryIO.readTableRows().withoutValidation().fromQuery(
+                        "SELECT * FROM `bigquery-public-data.austin_bikeshare.bikeshare_stations` LIMIT 10;"));
 
-        PCollection<TableRow> tableB = pipeline
-                .apply(
-                        "Read from BigQuery table B",
-                        BigQueryIO.readTableRows().withoutValidation().from(""));
+        PCollection<TableRow> tableB = pipeline.apply("Read from BigQuery table B",
+                BigQueryIO.readTableRows().withoutValidation().fromQuery(
+                        "SELECT * FROM `bigquery-public-data.san_francisco.bikeshare_stations` LIMIT 10;"));
 
-        PCollection<TableRow> tableC = pipeline
-                .apply(
-                        "Read from BigQuery table C",
-                        BigQueryIO.readTableRows().withoutValidation().from(""));
+        PCollection<TableRow> tableC = pipeline.apply("Read from BigQuery table C",
+                BigQueryIO.readTableRows().withoutValidation().fromQuery(
+                        "SELECT * FROM `bigquery-public-data.new_york_citibike.citibike_stations` LIMIT 10;"));
 
         // Flatten takes a PCollectionList of PCollection objects of a given type.
         // Returns a single PCollection that contains all of the elements in the PCollection objects in that list.
@@ -39,6 +39,17 @@ public class Main {
                 .of(tableA).and(tableB).and(tableC);
 
         PCollection<TableRow> mergedTables = collections.apply(Flatten.pCollections());
+
+        // Maps records to strings
+        mergedTables.apply(MapElements
+                .into(TypeDescriptors.strings())
+                .via(record -> record.toString())
+        )// Writes each window of records into GCS
+                .apply(TextIO
+                        .write()
+                        .to("merged_tables.txt")
+                        .withNumShards(1)
+                );
 
         // Select final schema
 
